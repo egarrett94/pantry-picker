98c8eaf2f0ad5c012cac908fbe4feee8e7f0f284
Lemon Garrett <e.marie.garrett@gmail.com>
2021-02-10 16:55:55 -0800

    extract search form into separate file and add emotion

--- client/src/App.js
+++ client/src/App.js
@@ -3,48 +3,29 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+import Nav from './components/Nav';
 import Home from './pages/Home';
 import LogIn from './pages/LogIn';
 import Recipe from './pages/Recipe';
 
-
-const App = () => {
-
-
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/recipe/:name">
-              <Recipe />
-            </Route>
-            <Route path="/login">
-              <LogIn />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/recipe/:name">
+            <Recipe />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+);
 
 export default App;
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,31 +1,58 @@
+/** @jsxImportSource @emotion/react */
+
 const SearchForm = ({handleSubmit, ingredients, handleIngredientsChange, handleDietChange, handleHealthChange }) => {
 
+    const formCss = {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: "0 auto",
+        backgroundColor: 'hotpink',
+        padding: '15px',
+        width: '30vw',
+        '> div': {
+            marginBottom: '10px'
+        },
+        '> input[type="text"], select': {
+            width: '100%!important'
+        },
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Enter an ingredient to find a recipe: </h2>
-            <input
-                type="text"
-                value={ingredients}
-                onChange={handleIngredientsChange}
-            />
+        <form css={formCss} onSubmit={handleSubmit}>
+            <h2>Enter an ingredient to find a recipe: </h2><br/>
+            <div>
+                <label htmlFor="ingredients">Enter ingredients separated by commas</label><br/>
+                <input
+                    type="text"
+                    name="ingredients"
+                    value={ingredients}
+                    onChange={handleIngredientsChange}
+                />
+            </div>
 
-            <select onChange={handleHealthChange}>
-                <option value="null" selected disabled>Health Options</option>
-                <option value="alcohol-free">Alcohol-Free</option>
-                <option value="peanut-free">Peanut-Free</option>
-                <option value="sugar-conscious">Sugar-Conscious</option>
-                <option value="tree-nut-free">Tree Nut-Free</option>
-                <option value="vegan">Vegan</option>
-                <option value="vegatarian">Vegetarian</option>
-            </select>
+            <div>
+                <label htmlFor="health">Select a health category</label><br/>
+                <select defaultValue="null" name="health" onChange={handleHealthChange}>
+                    <option value="null" disabled>Health Options</option>
+                    <option value="alcohol-free">Alcohol-Free</option>
+                    <option value="peanut-free">Peanut-Free</option>
+                    <option value="sugar-conscious">Sugar-Conscious</option>
+                    <option value="tree-nut-free">Tree Nut-Free</option>
+                    <option value="vegan">Vegan</option>
+                    <option value="vegatarian">Vegetarian</option>
+                </select>
+            </div>
 
-            <select onChange={handleDietChange}>
-                <option value="null" selected disabled>Diet Options</option>
-                <option value="balanced">Balanced</option>
-                <option value="high-protein">High Protein</option>
-                <option value="low-fat">Low Fat</option>
-                <option value="low-carb">Low Carb</option>
-            </select>
+            <div>
+                <label htmlFor="diet">Select a diet category</label><br/>
+                <select defaultValue="null" name="diet" onChange={handleDietChange}>
+                    <option value="null" disabled>Diet Options</option>
+                    <option value="balanced">Balanced</option>
+                    <option value="high-protein">High Protein</option>
+                    <option value="low-fat">Low Fat</option>
+                    <option value="low-carb">Low Carb</option>
+                </select>
+            </div>
 
             <button type="submit">Submit</button>
         </form>
